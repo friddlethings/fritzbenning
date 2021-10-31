d71d07d3f11581b5f18dcf2e7a5544c764bffee2
Fritz Benning <benning@neoskop.de>
2021-10-31 13:35:58 +0100

    feat: teaser

--- src/react-bricks/bricks/index.ts
+++ src/react-bricks/bricks/index.ts
@@ -1,8 +1,9 @@
 import { types } from 'react-bricks'
+import HeroImage from './HeroImage'
 import HeroUnit from './MyHeroUnit'
 import PageStage from './PageStage'
 import Richtext from './Richtext'
 
-const bricks: types.Brick<any>[] = [HeroUnit, Richtext, PageStage]
+const bricks: types.Brick<any>[] = [HeroUnit, Richtext, PageStage, HeroImage]
 
 export default bricks
