f4e6de10c112cf2424ddd9c52bf4afd53c1c007a
Fritz Benning <benning@neoskop.de>
2021-12-26 18:03:49 +0100

    feat: post-gallery

--- src/react-bricks/bricks/PostGallery/index.tsx
+++ src/react-bricks/bricks/PostGallery/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { types, usePagesPublic } from 'react-bricks/frontend'
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
 import Column from '../../../components/Grid/Column'
 import Row from '../../../components/Grid/Row'
 import Pagination from '../../../components/Pagination'
@@ -18,6 +19,7 @@ const PostGallery: types.Brick<PostGalleryProps> = ({
   pagination,
   pageSize,
 }) => {
+  const galleryRef = useRef(null)
   const [page, setPage] = useState(1)
   const { data } = usePagesPublic({
     type: 'post',
@@ -29,43 +31,59 @@ const PostGallery: types.Brick<PostGalleryProps> = ({
   })
 
   const changePage = (newPage: number) => {
-    window.scrollTo({ top: 0, behavior: 'smooth' })
+    window.scrollTo({
+      top: galleryRef.current.offsetTop - 80,
+      behavior: 'smooth',
+    })
     setPage(newPage)
+    console.log(galleryRef.current.offsetTop)
   }
 
   return (
     <Unit className="teaser-gallery" paddingTop>
-      <Row withVerticalGap>
-        {Array.isArray(data) &&
-          data.map((post) => (
-            <Column xs={12} m={6}>
-              <Teaser
-                title={post.name}
-                image={post.meta.featuredImage}
-                tags={post.tags}
-                to={`/${post.slug}`}
-              />
-            </Column>
-          ))}
-        {Array.isArray(data?.items) &&
-          data.items.map((post) => (
-            <Column xs={12} m={6}>
-              <Teaser
-                title={post.name}
-                image={post.meta.featuredImage}
-                tags={post.tags}
-                to={`/${post.slug}`}
-              />
-            </Column>
-          ))}
-      </Row>
-      {data && pagination && (
-        <Pagination
-          total={data.pagination.totalPages}
-          currentPage={page}
-          changePage={changePage}
-        />
-      )}
+      <div ref={galleryRef}>
+        <SwitchTransition mode="out-in">
+          <CSSTransition
+            key={`page-${page}`}
+            addEndListener={(node, done) => {
+              node.addEventListener('transitionend', done, false)
+            }}
+            classNames="fade"
+          >
+            <Row withVerticalGap>
+              {Array.isArray(data) &&
+                data.map((post) => (
+                  <Column xs={12} m={6}>
+                    <Teaser
+                      title={post.name}
+                      image={post.meta.featuredImage}
+                      tags={post.tags}
+                      to={`/${post.slug}`}
+                    />
+                  </Column>
+                ))}
+              {Array.isArray(data?.items) &&
+                data.items.map((post) => (
+                  <Column xs={12} m={6}>
+                    <Teaser
+                      title={post.name}
+                      image={post.meta.featuredImage}
+                      tags={post.tags}
+                      to={`/${post.slug}`}
+                    />
+                  </Column>
+                ))}
+            </Row>
+          </CSSTransition>
+        </SwitchTransition>
+        {data && pagination && (
+          <Pagination
+            total={data.pagination.totalPages}
+            currentPage={page}
+            changePage={changePage}
+          />
+        )}
+      </div>
     </Unit>
   )
 }
