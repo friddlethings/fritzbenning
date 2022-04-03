ee1f5d969723412e0ced9439fb9c0f52947deea3
Fritz Benning <benning@neoskop.de>
2022-04-03 13:19:13 +0200

    refactor: styling

--- next.config.js
+++ next.config.js
@@ -15,5 +15,39 @@ module.exports = {
   reactStrictMode: false,
   eslint: {
     dirs: ['react-bricks', 'components', 'pages', 'utils']
+  },
+  webpack(config /* ,{ buildId, dev, isServer, defaultLoaders, webpack }*/) {
+    const rules = config.module.rules
+      .find(rule => typeof rule.oneOf === 'object')
+      .oneOf.filter(rule => Array.isArray(rule.use))
+
+    rules.forEach(rule => {
+      rule.use.forEach(moduleLoader => {
+        if (
+          moduleLoader.loader?.includes('css-loader') &&
+          !moduleLoader.loader?.includes('postcss-loader')
+        )
+          moduleLoader.options.modules.getLocalIdent = (
+            context,
+            _,
+            exportName
+          ) => {
+            const rootClass = path
+              .relative(context.rootContext, path.dirname(context.resourcePath))
+              .split('/')
+              .slice(-1)
+              .join('-')
+              .toLowerCase()
+
+            if (rootClass !== exportName) {
+              return `${rootClass}-${exportName}`
+            } else {
+              return `${exportName}`
+            }
+          }
+      })
+    })
+
+    return config
   }
 }
